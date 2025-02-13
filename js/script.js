3449dedbe9ca05e85c72474e2266c04457b98323
Lakshara-Anand <lakshara77777@gmail.com>
2025-02-13 23:28:29 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function updateDate() {
+    const now = new Date();
+    const options = { 
+        weekday: 'long', 
+        year: 'numeric', 
+        month: 'long', 
+        day: 'numeric' 
+    };
+    const dateString = now.toLocaleDateString('en-US', options);
+    document.getElementById('date').textContent = dateString;
+}
+
+window.onload = function() {
+    updateDate();
+};
